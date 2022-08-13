2b3074d4ec9103a5eec55d64050c20bcc12567e8
Deepanjan Nag <subscriptions@deepanjannag.com>
2022-08-12 20:47:16 -0400

    Add project files.

---
+++ ToDoFluentValidation/Controllers/TasksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoFluentValidation.Models;
+
+namespace ToDoFluentValidation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+
+        [HttpPost]
+        public TaskItem Post(TaskItem model)
+        {
+            return model;
+        }
+    }
+}
---
+++ ToDoFluentValidation/Models/TaskItem.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+namespace ToDoFluentValidation.Models
+{
+    public class TaskItem
+    {
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool RemindMe { get; set; }
+        public int? ReminderMinutesBeforeDue { get; set; }
+        public List<string> SubItems { get; set; }
+    }
+
+    public class TaskItemValidator: AbstractValidator<TaskItem>
+    {
+        public TaskItemValidator()
+        {
+            RuleFor(t => t.Description).NotEmpty();
+
+            RuleFor(t => t.DueDate)
+                .GreaterThanOrEqualTo(DateTime.Today)
+                .WithMessage("DueDate must be in the future");
+
+            When(t => t.RemindMe == true, () =>
+                {
+                    RuleFor(t => t.ReminderMinutesBeforeDue)
+                        .NotNull()
+                        .WithMessage("ReminderMinutesBeforeDue must be set")
+                        .GreaterThan(0)
+                        .WithMessage("ReminderMinutesBeforeDue must be > 0")
+                        .Must(value => value % 15 == 0)
+                        .WithMessage("ReminderMinutesBeforeDue must be multiple of 15");
+                });
+
+            RuleForEach(t => t.SubItems)
+                .NotEmpty()
+                .WithMessage("Values in the SubItems array cannot be empty.");
+        }
+    }
+}
